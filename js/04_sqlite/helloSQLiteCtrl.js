41a6e8cc93080362402b344fa4882ea526c0b9c6
KemingChen <believe75467@gmail.com>
2014-03-23 22:56:13 +0800

    修改架構

--- js/04_sqlite/helloSQLiteCtrl.js
+++ js/04_sqlite/helloSQLiteCtrl.js
@@ -1,91 +1,48 @@
-app.controller('HelloSQLiteCtrl', function($scope, DBManager, Notification, Contacts) {
-	$scope.CREATE = 0;
-	$scope.EDIT = 1;
-	$scope.DELETE = 2;
+app.controller('HelloSQLiteCtrl', function($scope, $ionicModal, DBManager, Notification, Contacts) {
+	$scope.CREATE: 0;
+	$scope.EDIT: 1;
+	$scope.DELETE: 2;
 
-	$scope.friendArray;
+	$scope.friendArray = [];
 	$scope.model = {};
 	$scope.selectedIndex;
-	$scope.state = $scope.CREATE;
+	$scope.state = state.CREATE;
 	
-	$scope.init = function() {
-		$scope.friendArray = [];
-		DBManager.getFriends($scope.getFriendsSuccess);
-    };
-    
-	$scope.getFriendsSuccess = function (tx, res) {
+	$scope.rightButtons = [{
+		type: 'button-positive',
+		content: "<i class='icon ion-plus'></i>",
+		tap: setFriendsFromContacts
+	}];
+
+	$ionicModal.fromTemplateUrl('EditFriend.html', function(modal) {
+    	$scope.modal = modal;
+	}, {
+		scope: $scope,
+		animation: 'slide-in-up'
+	});
+
+	function getFriendsSuccess(tx, res) {
 		for (var i = 0, max = res.rows.length; i < max; i++) {
 			$scope.friendArray.push(res.rows.item(i));
 		}
 	};
 	
-	$scope.onCreateClick = function() {
-		if (!$scope.model.name || !$scope.model.phone) {
-			Notification.alert("請輸入姓名及電話", null, '警告', '確定');
-			return;
-		}
-		var friend = {
-			name: $scope.model.name,
-			phone: $scope.model.phone,
-			email: $scope.model.email,
-			birthday: $scope.model.birthday
-		};
-		DBManager.addFriend(friend, function() {
-			$scope.friendArray.push(friend);
-	    });
-		$scope.model = {};
-	};
-	
-	$scope.onEditFriendClick = function(index) {
-		$scope.state = $scope.EDIT;
-		$scope.model = angular.copy($scope.friendArray[index]);
-		$scope.selectedIndex = index;
-	};
-	
-	$scope.onEditClick = function() {
-		var friend = angular.copy($scope.model);
-		DBManager.updateFriend(friend, function() {
-			$scope.friendArray[$scope.selectedIndex] = friend;
-		});
-		$scope.model = {};
-		$scope.state = $scope.CREATE;
-	};
-	
-	$scope.onDeleteFriendClick = function(index) {
-		$scope.state = $scope.DELETE;
-		$scope.model = angular.copy($scope.friendArray[index]);
-		$scope.selectedIndex = index;
-	};
-	
-	$scope.onDeleteClick = function() {
-		DBManager.deleteFriend($scope.model.id, function() {
-			$scope.friendArray.splice($scope.selectedIndex, 1);
-		});
-		$scope.model = {};
-		$scope.state = $scope.CREATE;
-	};
-	
-	$scope.onCancelClick = function() {
-		$scope.model = {};
-		$scope.state = $scope.CREATE;
-	};
-	
-	$scope.setFriendsFromContacts = function() {
+	function setFriendsFromContacts() {
         var options = new ContactFindOptions();
         options.multiple = true;
         options.filter = "";
         var fields = ["displayName", "phoneNumbers", "emails", "birthday"];
-        Contacts.find(fields, $scope.onSetFriendsFromContactsSuccess, $scope.onSetFriendsFromContactsError, options);
+        Contacts.find(fields, onSetFriendsFromContactsSuccess, showError, options);
 	};
 
-    $scope.onSetFriendsFromContactsSuccess = function(contactArray) {
+    function onSetFriendsFromContactsSuccess(contactArray) {
+    	//console.log(JSON.stringify(contactArray));
         for (var i = 0, max = contactArray.length; i < max; i++) {
         	var contactName = contactArray[i].displayName;
-        	if (!contactName)
-        		continue;
         	var mobileNumber = getMobileNumber(contactArray[i].phoneNumbers);
-        	if (!mobileNumber)
+        	if (!contactName || !mobileNumber)
         		continue;
+
             var friend = {
                 name: contactName,
                 phone: mobileNumber,
@@ -97,7 +54,7 @@ app.controller('HelloSQLiteCtrl', function($scope, DBManager, Notification, Cont
         $scope.init();
     };
 	
-	var getMobileNumber = function(phoneNumbers) {
+	function getMobileNumber(phoneNumbers) {
 		if (!(phoneNumbers instanceof Array))
 			return null;
 		for (var i = 0, max = phoneNumbers.length; i < max; i++) {
@@ -107,13 +64,67 @@ app.controller('HelloSQLiteCtrl', function($scope, DBManager, Notification, Cont
 		return null;
 	};
     
-    $scope.onSetFriendsFromContactsError = function(e) {
-        console.log(e);
+    function showError(e) {
+        console.log(JSON.stringify(e));
     };
-	    
-	$scope.newFriendsButton = [{
-		type: 'button-positive',
-		content: "<i class='icon ion-plus'></i>",
-		tap: $scope.setFriendsFromContacts
-	}];
+
+    $scope.init = function(){
+    	$scope.friendArray = [];
+    	DBManager.getFriends(getFriendsSuccess);
+    };
+
+    $scope.onCreateClick = function() {
+		if (!$scope.model.name || !$scope.model.phone) {
+			Notification.alert("請輸入姓名及電話", null, '警告', '確定');
+			return;
+		}
+
+		var friend = angular.copy($scope.model);
+		$scope.model = {};
+
+		DBManager.addFriend(friend, function() {
+			$scope.friendArray.push(friend);
+	    });
+		$scope.modal.hide();
+	};
+	
+	$scope.onEditClick = function() {
+		var friend = angular.copy($scope.model);
+		$scope.model = {};
+		$scope.state = state.CREATE;
+
+		DBManager.updateFriend(friend, function() {
+			$scope.friendArray[$scope.selectedIndex] = friend;
+		});
+		$scope.modal.hide();
+	};
+	
+	$scope.onDeleteFriendClick = function(index) {
+		$scope.state = state.DELETE;
+		$scope.model = angular.copy($scope.friendArray[index]);
+		$scope.selectedIndex = index;
+		$scope.modal.show();
+	};
+	
+	$scope.onEditFriendClick = function(index) {
+		$scope.state = state.EDIT;
+		$scope.model = angular.copy($scope.friendArray[index]);
+		$scope.selectedIndex = index;
+		$scope.modal.show();
+	};
+	
+	$scope.onDeleteClick = function() {
+		DBManager.deleteFriend($scope.model.id, function(){
+			$scope.friendArray.splice($scope.selectedIndex, 1);
+		});
+		$scope.model = {};
+		$scope.state = state.CREATE;
+		$scope.modal.hide();
+	};
+	
+	$scope.onCancelClick = function() {
+		$scope.model = {};
+		$scope.state = $scope.CREATE;
+		$scope.modal.hide();
+	};
 });
